0dd9690e3c8dee5a6289e1cdb3f2f3d2756ab878
MiXu <michael.schindler894@gmail.com>
2022-12-12 10:07:01 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Socials from "./components/Socials/Socials";
+import Sub from "./components/Sub/Sub";
+import Tabs from "./components/Tabs/Tabs";
+import Standard from "./Standard Pictures/Standard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <Socials />
+      <Tabs />
+      <Sub />
+      <Standard />
+      <Footer />
     </div>
   );
 }
